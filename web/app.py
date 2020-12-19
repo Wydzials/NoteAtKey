1f114d68c69b6f49fe65e9a6b2dc4f21d6dad71d
Wydzials <szymonstefan1999@gmail.com>
2020-12-19 13:33:59 +0100

    Add docker-compose configuration

---
+++ web/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from redis import Redis
+
+from os import getenv
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+load_dotenv()
+
+cloud_url = getenv("REDIS_URL")
+db = Redis.from_url(cloud_url, decode_responses=True) if cloud_url else Redis(host="redis", decode_responses=True)
+
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! ' + str(db.get("test"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
