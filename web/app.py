d715da16733b9df607020a4bc8aba7caa45fde13
Wydzials <szymonstefan1999@gmail.com>
2020-12-19 16:47:57 +0100

    Add register and login pages

--- web/app.py
+++ web/app.py
@@ -18,6 +18,14 @@ app.config.from_object(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
